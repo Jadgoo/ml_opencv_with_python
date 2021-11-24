90f6cf7f53b51c75703ccfb8e9e0677f5470c687
lawrence <yutsn1996@icloud.com>
2021-11-24 22:19:09 +0800

    First commit!

---
+++ testGit/testGit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testGit
+//
+//  Created by Lawrence Zheng on 2021/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
